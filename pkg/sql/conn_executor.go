ac831d7a87f4ac88ba4223bff5967a19b7d9c6a9
Thomas Hardy <thardy@cockroachlabs.com>
2023-04-28 15:53:42 -0400

    remove connExecutor usage of initialization

--- pkg/sql/conn_executor.go
+++ pkg/sql/conn_executor.go
@@ -3490,7 +3490,6 @@ func (ex *connExecutor) initPlanner(ctx context.Context, p *planner) {
 	p.schemaResolver.sessionDataStack = p.EvalContext().SessionDataStack
 	p.schemaResolver.descCollection = p.Descriptors()
 	p.schemaResolver.authAccessor = p
-	p.initializeReducedAuditConfig(ctx)
 }
 
 func (ex *connExecutor) resetPlanner(
